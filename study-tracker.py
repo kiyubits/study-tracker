0da8190e17e8f657c79c819589115f02f10d0152
Kiyubits <richard4pech@gmail.com>
2024-06-06 00:16:03 +1000

    add affirming phrases

--- study-tracker.py
+++ study-tracker.py
@@ -3,6 +3,7 @@ import json
 import os
 import threading
 import time
+import random
 
 def calculate_session_length(start, end):
     session_length = end - start
@@ -39,6 +40,18 @@ def track_study_session():
 
     input()
 
+    affirmations = [
+        "You did really well today!",
+        "Good job today :)",
+        "Keep going, you're improving everyday!",
+        "I hope you had a good session hehe",
+        "You are filled with determination.",
+        "Well done! You deserved this.",
+        "やった！",
+        "Study hard what interests you the most in the most undisciplined, irreverent and original manner possible.",
+        "The first principle is that you must not fool yourself and you are the easiest person to fool."
+    ]
+
     print(f"Doing Physics Everyday Until I Graduate University | Day {day_number.days}")
     print(f"Session started at: {start_time_str}")
     end_time = datetime.datetime.now()
@@ -50,7 +63,7 @@ def track_study_session():
     hours, remainder = divmod(session_length.total_seconds(), 3600)
     minutes, seconds = divmod(remainder, 60)
     session_length_str = f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
-    print(f"Session length: {session_length_str}")
+    print(f"Session length: {session_length_str}\n")
 
     session_data = {
         "day": day_number.days,
@@ -73,7 +86,7 @@ def track_study_session():
     with open(file_name, 'w') as file:
         json.dump(data, file, indent=4)
 
-    print("Session data saved successfully.")
+    print(affirmations[random.randint(0, len(affirmations) - 1)])
 
 if __name__ == "__main__":
     track_study_session()
