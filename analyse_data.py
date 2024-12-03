33f6c2421d9dadac6d7ce4838cf0c9d0d9e13012
Kiyubits <95125913+kiyubits@users.noreply.github.com>
2024-12-03 16:52:41 +1100

    Day 6 - Coding

--- analyse_data.py
+++ analyse_data.py
@@ -4,14 +4,26 @@ from utils import time_str_to_timedelta
 
 # Load JSON data
 with open('study_sessions.json', 'r') as file:
-    data = json.load(file)
+    study_data = json.load(file)
 
 # Calculate total session length
-total_session_length = timedelta()
-for day in data:
+total_study_session_length = timedelta()
+for day in study_data:
     for session in day["sessions"]:
-        total_session_length += time_str_to_timedelta(session["session_length"])
+        total_study_session_length += time_str_to_timedelta(session["session_length"])
 
 # Print total session length
-print("Total hours spent studying physics:", total_session_length)
+print("Total hours spent studying physics:", total_study_session_length)
 
+# Load JSON data
+with open('coding_sessions.json', 'r') as file:
+    code_data = json.load(file)
+
+# Calculate total session length
+total_code_session_length = timedelta()
+for day in code_data:
+    for session in day["sessions"]:
+        total_code_session_length += time_str_to_timedelta(session["session_length"])
+
+# Print total session length
+print("Total hours spent coding:", total_code_session_length)
